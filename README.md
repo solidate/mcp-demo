10ee9dc1bddb9dfe7836fcab4e07a16e5648b985
keshv <keshv@google.com>
2025-05-17 09:57:53 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# mcp-demo
-Scrappy start with MCP
+# Scrappy MCP Demo
+
+This demo creates a MCP server which has tools for performing inventory operations
+
+Demo steps:
+1. Open two terminal
+2. In first Terminal:
+    ```
+    uv run operations.py
+    ```
+3. In second terminal start MCP inspector:
+    ```
+    npx @modelcontextprotocol/inspector
+    ```
---
+++ agent.py
@@ -0,0 +1,133 @@
+
+from collections.abc import AsyncIterable
+from typing import Any, Literal
+
+import httpx
+import asyncio
+
+from langchain_core.messages import AIMessage, ToolMessage
+from langchain_core.tools import tool
+from langchain_google_vertexai import ChatVertexAI
+from pydantic import BaseModel
+
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+
+async def load_mcp_tools():
+    client = MultiServerMCPClient(
+        {
+            "inventory": {
+                # Ensure your start your inventory server on port 4200
+                "url": "http://localhost:4200/inventory",
+                "transport": "streamable_http",
+            }
+        }
+    )
+    tools = await client.get_tools()
+    return tools
+
+memory = MemorySaver()
+
+
+class ResponseFormat(BaseModel):
+    """Respond to the user in this format."""
+
+    status: Literal['input_required', 'completed', 'error'] = 'input_required'
+    message: str
+
+
+class InventoryAgent:
+    
+    def __init__(self):
+        self.model = ChatVertexAI(model='gemini-2.0-flash-001')
+        self.tools = asyncio.run(load_mcp_tools())
+        SYSTEM_INSTRUCTION = (
+        'You are a specialized assistant for inventory management.'
+        f'Your purpose is to use the available tools: {self.tools} to answer questions about'
+        'SKUs and items.'
+    )
+        self.SYSTEM_INSTRUCTION = SYSTEM_INSTRUCTION
+
+        self.graph = create_react_agent(
+            self.model,
+            tools=self.tools,
+            checkpointer=memory,
+            prompt=self.SYSTEM_INSTRUCTION,
+            response_format=ResponseFormat,
+        )
+
+    def invoke(self, query, sessionId) -> str:
+        config = {'configurable': {'thread_id': sessionId}}
+        self.graph.invoke({'messages': [('user', query)]}, config)
+        return self.get_agent_response(config)
+    
+    async def ainvoke(self, query, sessionId) -> str:
+        config = {'configurable': {'thread_id': sessionId}}
+        await self.graph.ainvoke({'messages': [('user', query)]}, config)
+        return self.get_agent_response(config)
+
+    async def stream(self, query, sessionId) -> AsyncIterable[dict[str, Any]]:
+        inputs = {'messages': [('user', query)]}
+        config = {'configurable': {'thread_id': sessionId}}
+
+        for item in self.graph.stream(inputs, config, stream_mode='values'):
+            message = item['messages'][-1]
+            if (
+                isinstance(message, AIMessage)
+                and message.tool_calls
+                and len(message.tool_calls) > 0
+            ):
+                yield {
+                    'is_task_complete': False,
+                    'require_user_input': False,
+                    'content': 'Looking up the exchange rates...',
+                }
+            elif isinstance(message, ToolMessage):
+                yield {
+                    'is_task_complete': False,
+                    'require_user_input': False,
+                    'content': 'Processing the exchange rates..',
+                }
+
+        yield self.get_agent_response(config)
+
+    def get_agent_response(self, config):
+        current_state = self.graph.get_state(config)
+        structured_response = current_state.values.get('structured_response')
+        if structured_response and isinstance(
+            structured_response, ResponseFormat
+        ):
+            if (
+                structured_response.status == 'input_required'
+                or structured_response.status == 'error'
+            ):
+                return {
+                    'is_task_complete': False,
+                    'require_user_input': True,
+                    'content': structured_response.message,
+                }
+            if structured_response.status == 'completed':
+                return {
+                    'is_task_complete': True,
+                    'require_user_input': False,
+                    'content': structured_response.message,
+                }
+
+        return {
+            'is_task_complete': False,
+            'require_user_input': True,
+            'content': 'We are unable to process your request at the moment. Please try again.',
+        }
+
+    SUPPORTED_CONTENT_TYPES = ['text', 'text/plain']
+    
+if __name__=="__main__":
+    agent = InventoryAgent()
+    response = asyncio.run(agent.ainvoke(
+        "What all items are available?",
+        3
+    ))
+    print(response)
+
+
